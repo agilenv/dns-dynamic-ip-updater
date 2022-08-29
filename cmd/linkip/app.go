fc9235373b5332b68e874d750b589b9fcbf72dfe
Facundo Lopiano <facundolopiano@gmail.com>
2022-08-29 11:09:58 -0300

    Add env-file parameter (#7)

--- cmd/linkip/app.go
+++ cmd/linkip/app.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/urfave/cli/v2"
+)
+
+const (
+	envFilepath = ".env"
 )
 
 func buildApp() *cli.App {
 	u := buildUpdater()
-
+	envFile := envFilepath
 	return &cli.App{
 		Name:  "linkip",
 		Usage: "Simple application to keep updated a dns record where the ip associated is dynamic",
 		Commands: []*cli.Command{
 			statusCMD(u),
 			updateCMD(u),
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "env-file",
+				Usage:       "path to env file",
+				Destination: &envFile,
+				Value:       envFile,
+				HasBeenSet:  true,
+			},
+		},
+		Before: func(cCtx *cli.Context) error {
+			if err := godotenv.Load(envFile); err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+				return errors.New("missing .env file. You can provide a file path with the flag --env-file")
+			}
+			return nil
 		},
 	}
 }
